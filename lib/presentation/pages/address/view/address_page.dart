e815f0ceec803918a6572886b153295126dcf8a8
Saidmirzo <saidmirzobahromov88@gmail.com>
2022-11-04 01:06:25 +0500

    added address page

---
+++ lib/presentation/pages/address/view/address_page.dart
@@ -0,0 +1,307 @@
+import 'dart:async';
+import 'dart:math';
+import 'dart:ui' as ui;
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:jbaza/jbaza.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
+import 'package:takk/config/constants/app_colors.dart';
+import 'package:takk/config/constants/app_text_styles.dart';
+import 'package:takk/core/di/app_locator.dart';
+import 'package:takk/data/models/cart_response.dart';
+import 'package:takk/presentation/pages/address/viewmodel/address_viewmodel.dart';
+
+import '../../../../data/models/address_models/address_model.dart';
+import '../../../../data/models/address_models/location_result.dart';
+import '../../../../data/models/cafe_model/location.dart';
+import '../../../widgets/info_dialog.dart';
+import '../../../widgets/loading_dialog.dart';
+
+class AddressPage extends ViewModelBuilderWidget<AddressViewModel> {
+  AddressPage(this.cafeLocation, this.maxRadius, this.instruction);
+
+  final Location cafeLocation;
+  final double maxRadius;
+  final String instruction;
+
+  final String tag = 'AddressPage';
+  FloatingSearchBarController floatingSearchBarController =
+      FloatingSearchBarController();
+  Completer<GoogleMapController> _controller = Completer();
+  Set<Circle> _circle = Set();
+
+  @override
+  void onViewModelReady(AddressViewModel viewModel) {
+    super.onViewModelReady(viewModel);
+    viewModel.currentPosition =
+        LatLng(cafeLocation.coordinates![0], cafeLocation.coordinates![1]);
+    _circle.add(Circle(
+      circleId: CircleId(tag),
+      center: viewModel.currentPosition!,
+      radius: maxRadius,
+      fillColor: AppColors.accentColor.withOpacity(0.5),
+      strokeColor: AppColors.accentColor,
+      strokeWidth: 2,
+    ));
+    viewModel
+        .reverseGeocodeLatLng(tag)
+        .then((value) => viewModel.addMarker(tag: tag, locationResult: value));
+    // Future.delayed(Duration.zero, () => showLoadingDialog(context));
+    viewModel.getUserLastAddresses(tag);
+  }
+
+  @override
+  void onDestroy(AddressViewModel model) {
+    floatingSearchBarController.dispose();
+    super.onDestroy(model);
+  }
+
+  Future<void> _setStyle(GoogleMapController controller) async {
+    controller
+        .setMapStyle(await rootBundle.loadString('assets/data/map_style.json'));
+  }
+
+  bool calculateRadius(LatLng latLng) {
+    return pow(cafeLocation.coordinates![0] - latLng.latitude, 2) +
+            pow(cafeLocation.coordinates![1] - latLng.longitude, 2) <=
+        pow(maxRadius / 1000 * 0.1988, 2);
+  }
+
+  @override
+  Widget builder(
+      BuildContext context, AddressViewModel viewModel, Widget? child) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            zoomGesturesEnabled: true,
+            initialCameraPosition: CameraPosition(
+              target: viewModel.currentPosition ??
+                  const LatLng(40.703504, -74.016594),
+              zoom: 15,
+            ),
+            mapToolbarEnabled: true,
+            compassEnabled: false,
+            myLocationEnabled: true,
+            myLocationButtonEnabled: false,
+            circles: _circle,
+            markers: viewModel.markers,
+            onMapCreated: (GoogleMapController controller) {
+              _setStyle(controller);
+              _controller.complete(controller);
+            },
+            onCameraMove: (position) {
+              viewModel.currentPosition = position.target;
+            },
+            onCameraMoveStarted: () {
+              viewModel.isMove = true;
+            },
+            onCameraIdle: () {
+              viewModel.funcCameraIdleMove(
+                  tag: tag, mapController: _controller);
+            },
+          ),
+          Positioned(
+            bottom: 15,
+            left: 10,
+            right: 10,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12), color: Colors.white),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  if (viewModel.isSuccess(tag: tag))
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: CircularProgressIndicator(),
+                    )
+                  else
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 5, right: 10),
+                        child: Text(
+                          viewModel.locationResult != null
+                              ? viewModel.locationResult!.address ??
+                                  'Unknown address'
+                              : 'Unknown address',
+                          style: AppTextStyles.body15w6,
+                        ),
+                      ),
+                    ),
+                  FloatingActionButton(
+                    onPressed: () {
+                      viewModel.funcFloatActionButton();
+                    },
+                    child: const Icon(Icons.arrow_forward),
+                  )
+                ],
+              ),
+            ),
+          ),
+          IgnorePointer(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Icon(Icons.place, size: 45),
+                  Container(
+                    decoration: const ShapeDecoration(
+                      shadows: [
+                        BoxShadow(
+                          blurRadius: 4,
+                          color: Colors.black38,
+                        ),
+                      ],
+                      shape: CircleBorder(
+                        side: BorderSide(
+                          width: 4,
+                          color: Colors.transparent,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 45),
+                ],
+              ),
+            ),
+          ),
+          FloatingSearchBar(
+            hint: 'Search...',
+            hintStyle: AppTextStyles.body16w5
+                .copyWith(color: AppColors.textColor.shade2),
+            scrollPadding: const EdgeInsets.only(top: 15, bottom: 56),
+            margins: const EdgeInsets.only(left: 15, right: 15, top: 45),
+            transitionDuration: const Duration(milliseconds: 500),
+            transitionCurve: Curves.easeInOut,
+            transition: CircularFloatingSearchBarTransition(),
+            physics: const BouncingScrollPhysics(),
+            openAxisAlignment: 0.0,
+            debounceDelay: const Duration(milliseconds: 300),
+            borderRadius: BorderRadius.circular(12),
+            controller: floatingSearchBarController,
+            elevation: 0,
+            height: 45,
+            iconColor: AppColors.textColor.shade1,
+            onQueryChanged: (query) {
+              if (query.length > 3) {
+                viewModel.autoCompleteSearch(tag, query);
+              }
+            },
+            actions: [
+              FloatingSearchBarAction(
+                showIfOpened: true,
+                child: viewModel.isBusy(tag: tag)
+                    ? Container(
+                        height: 40,
+                        width: 40,
+                        padding: const EdgeInsets.all(10),
+                        child: CircularProgressIndicator(
+                          strokeWidth: 1.5,
+                          valueColor: AlwaysStoppedAnimation<Color>(
+                              AppColors.textColor.shade1),
+                        ),
+                      )
+                    : CircularButton(
+                        icon: Icon(
+                          Icons.location_on_rounded,
+                          color: AppColors.textColor.shade1,
+                          size: 22,
+                        ),
+                        onPressed: () {
+                          viewModel.funcFloatSearchBarButton(_controller);
+                        }),
+              ),
+            ],
+            builder: (context, transition) {
+              return ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Material(
+                  color: Colors.white,
+                  elevation: 4.0,
+                  child: ListView.separated(
+                      shrinkWrap: true,
+                      padding: EdgeInsets.zero,
+                      itemBuilder: (context, index) =>
+                          floatingSearchBarController.query.isEmpty
+                              ? ListTile(
+                                  contentPadding:
+                                      const EdgeInsets.only(left: 15),
+                                  onTap: () {
+                                    viewModel.funcUserAddressItem1(
+                                        index,
+                                        floatingSearchBarController,
+                                        _controller);
+                                  },
+                                  title: Text(
+                                    viewModel
+                                            .listUserAddresses[index].address ??
+                                        'Unknown address',
+                                    style: AppTextStyles.body14w6,
+                                  ),
+                                  trailing: IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(
+                                      Icons.favorite_border,
+                                      size: 20,
+                                      color: AppColors.textColor.shade1,
+                                    ),
+                                    iconSize: 20,
+                                  ),
+                                )
+                              : ListTile(
+                                  contentPadding:
+                                      const EdgeInsets.only(left: 15),
+                                  onTap: () {
+                                    viewModel.funcUserAddressItem2(
+                                        tag,
+                                        index,
+                                        _controller,
+                                        floatingSearchBarController);
+                                  },
+                                  title: Text(
+                                    viewModel.completeAddresses[index].text ??
+                                        'Unknown address',
+                                    style: AppTextStyles.body14w6,
+                                  ),
+                                  trailing: IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(
+                                      Icons.favorite_border,
+                                      size: 20,
+                                      color: AppColors.textColor.shade1,
+                                    ),
+                                    iconSize: 20,
+                                  ),
+                                ),
+                      separatorBuilder: (context, index) => Divider(
+                            color: AppColors.textColor.shade2,
+                            height: 1,
+                          ),
+                      itemCount: floatingSearchBarController.query.isEmpty
+                          ? viewModel.listUserAddresses.length
+                          : viewModel.completeAddresses.length),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  AddressViewModel viewModelBuilder(BuildContext context) {
+    return AddressViewModel(
+        context: context,
+        addressRepository: locator.get(),
+        cafeLocation: cafeLocation,
+        instruction: instruction,
+        maxRadius: maxRadius);
+  }
+}
---
+++ lib/presentation/pages/address/viewmodel/address_viewmodel.dart
@@ -0,0 +1,248 @@
+import 'dart:async';
+import 'dart:math';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:jbaza/jbaza.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
+import 'package:takk/core/di/app_locator.dart';
+import 'package:takk/data/models/cafe_model/location.dart';
+import 'package:takk/domain/repositories/address_repository.dart';
+import 'package:takk/domain/repositories/user_repository.dart';
+import 'package:top_snackbar_flutter/custom_snack_bar.dart';
+import 'package:top_snackbar_flutter/top_snack_bar.dart';
+
+import '../../../../data/models/address_models/address_model.dart';
+import '../../../../data/models/address_models/auto_complete_model.dart';
+import '../../../../data/models/address_models/location_result.dart';
+import '../../../../data/models/cart_response.dart';
+import '../../../widgets/info_dialog.dart';
+import '../../../widgets/loading_dialog.dart';
+
+class AddressViewModel extends BaseViewModel {
+  AddressViewModel(
+      {required super.context,
+      required this.addressRepository,
+      required this.cafeLocation,
+      required this.maxRadius,
+      required this.instruction});
+  AddressRepository addressRepository;
+
+  Location cafeLocation;
+  double maxRadius;
+  String instruction;
+
+  Future? dialog;
+
+  List<AutoCompleteModel> completeAddresses = [];
+
+  List<AddressModel> listUserAddresses = [];
+
+  Set<Marker> markers = Set();
+
+  LocationResult? locationResult;
+
+  LatLng? currentPosition;
+
+  bool isMove = false;
+
+  final String addAddressTag = 'addAddressTag';
+
+  Future<LocationResult?> reverseGeocodeLatLng(String tag) async {
+    safeBlock(() async {
+      LocationResult? result = await addressRepository.reverseGeocodeLatLng(
+          tag, currentPosition ?? const LatLng(40.703504, -74.016594));
+      setSuccess(tag: tag);
+      return result;
+    }, callFuncName: 'reverseGeocodeLatLng');
+    return null;
+  }
+
+  Future addMarker(
+      {required String tag, required LocationResult? locationResult}) async {
+    Uint8List cafeIcon =
+        await getBytesFromAsset('assets/icons/ic_cafe_location.png', 90);
+    markers.add(Marker(
+        markerId: MarkerId(tag),
+        icon: BitmapDescriptor.fromBytes(cafeIcon),
+        position: currentPosition!));
+
+    locationResult = locationResult;
+  }
+
+  Future<Uint8List> getBytesFromAsset(String path, int width) async {
+    ByteData data = await rootBundle.load(path);
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
+        targetWidth: width);
+    ui.FrameInfo fi = await codec.getNextFrame();
+    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!
+        .buffer
+        .asUint8List();
+  }
+
+  void autoCompleteSearch(String tag, String place) async {
+    safeBlock(() async {
+      completeAddresses =
+          await addressRepository.autoCompleteSearch(tag, place) ?? [];
+
+      setSuccess(tag: tag);
+    }, callFuncName: 'autoCompleteSearch');
+  }
+
+  void getUserLastAddresses(String tag) async {
+    safeBlock(() async {
+      List<AddressModel>? list =
+          await addressRepository.getUserLastAddresses(tag);
+      if (list != null) listUserAddresses = list;
+      notifyListeners();
+    });
+  }
+
+  void funcCameraIdleMove(
+      {required String tag,
+      required Completer<GoogleMapController> mapController}) {
+    if (isMove) {
+      if (arePointsNear(checkPoint: currentPosition!)) {
+        reverseGeocodeLatLng(tag).then((value) {
+          locationResult = value;
+          notifyListeners();
+        });
+      } else {
+        showInfoDialog(context!, 'Out of the delivery range').then(
+            (value) => moveToCurrentLocation(mapController: mapController));
+      }
+      isMove = false;
+    }
+  }
+
+  Future moveToCurrentLocation({
+    LatLng? loc,
+    required Completer<GoogleMapController> mapController,
+  }) async {
+    final controller = await mapController.future;
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+            target: loc ??
+                LatLng(
+                    cafeLocation.coordinates![0], cafeLocation.coordinates![1]),
+            zoom: 15),
+      ),
+    );
+  }
+
+  bool arePointsNear({
+    required LatLng checkPoint,
+  }) {
+    var ky = 40000 / 360;
+    var kx = cos(pi * cafeLocation.coordinates![0] / 180.0) * ky;
+    double dx =
+        (cafeLocation.coordinates![1] - checkPoint.longitude).abs() * kx;
+    double dy = (cafeLocation.coordinates![0] - checkPoint.latitude).abs() * ky;
+    return sqrt(dx * dx + dy * dy) <= maxRadius / 1000;
+  }
+
+  void funcFloatActionButton() async {
+    safeBlock(() async {
+      if (locationResult != null) {
+        showLoadingDialog(context!);
+        addressRepository.addAddress(
+          addAddressTag,
+          DeliveryInfo(
+              id: 0,
+              address: locationResult!.address,
+              latitude: locationResult!.latLng!.latitude.toString(),
+              longitude: locationResult!.latLng!.longitude.toString(),
+              instruction: instruction),
+        );
+      }
+    }, callFuncName: 'funcFloatActionButton');
+  }
+
+  void funcFloatSearchBarButton(Completer<GoogleMapController> controller) {
+    safeBlock(() async {
+      LatLng? result = await locator<UserRepository>().getLocation();
+      if (result != null) {
+        if (arePointsNear(checkPoint: result)) {
+          currentPosition = result;
+          moveToCurrentLocation(
+              loc: currentPosition!, mapController: controller);
+        } else {
+          showInfoDialog(context!, 'Out of the delivery range').then(
+            (value) => moveToCurrentLocation(mapController: controller),
+          );
+        }
+      }
+    });
+  }
+
+  void funcUserAddressItem1(
+      int index,
+      FloatingSearchBarController floatingSearchBarController,
+      Completer<GoogleMapController> mapController) {
+    var l = LatLng(
+      double.parse(listUserAddresses[index].latitude ?? '0'),
+      double.parse(listUserAddresses[index].longitude ?? '0'),
+    );
+    if (arePointsNear(checkPoint: l)) {
+      floatingSearchBarController.close();
+      moveToCurrentLocation(loc: l, mapController: mapController);
+    } else {
+      showInfoDialog(context!, 'Out of the delivery range')
+          .then((value) => moveToCurrentLocation(mapController: mapController));
+    }
+  }
+
+  void funcUserAddressItem2(String tag, int index,Completer<GoogleMapController> mapController,FloatingSearchBarController floatingSearchBarController ) {
+    addressRepository
+        .decodeAndSelectPlace(tag, completeAddresses[index].id)
+        .then((value) {
+      if (value != null) {
+        if (arePointsNear(checkPoint:  value)) {
+          floatingSearchBarController.close();
+          moveToCurrentLocation(loc: value, mapController: mapController);
+        } else {
+          showInfoDialog(context!, 'Out of the delivery range')
+              .then((value) => moveToCurrentLocation( mapController: mapController));
+        }
+      }
+    });
+  }
+
+//--------------------------------------------------------------------------------------------
+  @override
+  callBackBusy(bool value, String? tag) {
+    if (isBusy(tag: tag)) {
+      Future.delayed(Duration.zero, () {
+        dialog = showLoadingDialog(context!);
+      });
+    } else {
+      if (dialog != null) {
+        pop();
+        dialog = null;
+      }
+    }
+  }
+
+  @override
+  callBackSuccess(value, String? tag) {
+    if (dialog != null) {
+      pop();
+      dialog = null;
+    }
+  }
+
+  @override
+  callBackError(String text) {
+    if (dialog != null) pop();
+    showTopSnackBar(
+      context!,
+      CustomSnackBar.error(
+        message: text,
+      ),
+    );
+  }
+}
